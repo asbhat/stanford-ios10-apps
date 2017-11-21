91e519b63459a390694b1cafac3d280c238e0802
Aditya <aditya.s.bhat@gmail.com>
2017-11-20 18:56:59 -0800

    Project 3 Extra Credit 2 - persist scale & origin

--- Calculator/Calculator/GraphingViewController.swift
+++ Calculator/Calculator/GraphingViewController.swift
@@ -26,17 +26,6 @@ class GraphingViewController: UIViewController {
         return true
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     private var graphingModel = GraphingModel() {
         didSet {
             updateGraphingViewFunction()
@@ -69,15 +58,24 @@ class GraphingViewController: UIViewController {
         graphingModel.equation = equation
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let savedGraphScale = CGFloat(UserDefaults.standard.double(forKey: GraphDefaultKeys.scale))
+        if savedGraphScale != 0 {
+            graphingView.scale = savedGraphScale
+        }
+        let savedGraphOrigin = CGPoint(x: UserDefaults.standard.double(forKey: GraphDefaultKeys.originX), y: UserDefaults.standard.double(forKey: GraphDefaultKeys.originY))
+        if savedGraphOrigin != CGPoint.zero {
+            graphingView.origin = savedGraphOrigin
+        }
     }
-    */
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        UserDefaults.standard.set(Double(graphingView.scale), forKey: GraphDefaultKeys.scale)
+        UserDefaults.standard.set(Double(graphingView.origin.x), forKey: GraphDefaultKeys.originX)
+        UserDefaults.standard.set(Double(graphingView.origin.y), forKey: GraphDefaultKeys.originY)
+    }
 
     override func viewWillLayoutSubviews() {
          graphingView.saveOriginToCenterDifference()
@@ -85,5 +83,10 @@ class GraphingViewController: UIViewController {
     override func viewDidLayoutSubviews() {
          graphingView.applyOriginToCenterDifference()
     }
+}
 
+fileprivate struct GraphDefaultKeys {
+    static let scale = "graphScale"
+    static let originX = "graphOriginX"
+    static let originY = "graphOriginY"
 }
